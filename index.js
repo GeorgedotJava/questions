7616905bddeefcf42f31b7de93c7e0db44494fda
George Lucas Vieira Dos Santos <geolucas3@gmail.com>
2021-02-27 22:40:51 -0300

    Alteraçao do front-end e sequelize download

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname, 'views'))
-
+app.set('views', path.resolve(__dirname, 'views'));
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/perguntar', (req, res) => {
+    res.render('perguntar');
+});
+
+app.post('/salvarpergunta', (req, res) => {
+    let nome = req.body.titulo;
+    let descricao = req.body.descricao;
+    
+    res.send('Formulário enviado');
 })
 
 
 
-
-
-app.listen(3000, (e) => {
+app.listen(3001, (e) => {
     if(e){
         console.log(`ERRO: ${e}`);
     } else {
-        console.log('Server on port 3000');
+        console.log('Server on port 3001');
     }
 })
